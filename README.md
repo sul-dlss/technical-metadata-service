fcb91bf4bb8c2324f434f3cebd8e30298b453577
Justin Littman <justinlittman@stanford.edu>
2020-02-18 11:46:02 -0500

    Adds siegfried for file indentification.
    closes #28

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ Background processing is performed by [Sidekiq](https://github.com/mperham/sidek
 
 Sidekiq can be monitored from [/queues](http://localhost:3000/queues).
 For more information on configuring and deploying Sidekiq, see this [doc](https://github.com/sul-dlss/DevOpsDocs/blob/master/projects/sul-requests/background_jobs.md).
+
+## Requirements
+### Siegfried
+[Siegfried](https://github.com/richardlehane/siegfried) is used for file identification.
+
+To install on OS X:
+```
+brew install mistydemeo/digipres/siegfried
+```
--- spec/services/technical_metadata_generator_spec.rb
+++ spec/services/technical_metadata_generator_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe TechnicalMetadataGenerator do
 
   let(:druid) { 'druid:abc123' }
 
+  let(:file_identifier_service) { instance_double(FileIdentifierService, version: '1.4.5') }
+
+  before do
+    allow(FileIdentifierService).to receive(:new).and_return(file_identifier_service)
+    allow(file_identifier_service).to receive(:identify).with(filepath: 'spec/fixtures/test/0001.html').and_return('text/html')
+    allow(file_identifier_service).to receive(:identify).with(filepath: 'spec/fixtures/test/bar.txt').and_return('text/plain')
+    allow(file_identifier_service).to receive(:identify).with(filepath: 'spec/fixtures/test/foo.txt').and_return('text/plain')
+  end
+
   describe '#generate' do
     context 'when all files exist' do
       let(:filepaths) do
@@ -19,7 +28,7 @@ RSpec.describe TechnicalMetadataGenerator do
 
       before do
         # Unchanged
-        DroFile.create(druid: druid, filename: '0001.html', md5: 'd41d8cd98f00b204e9800998ecf8427e', bytes: 0,
+        DroFile.create(druid: druid, filename: '0001.html', md5: '1711cb9f08a0504e1035d198d08edda9', bytes: 0,
                        filetype: 'test')
         # MD5 mismatch
         DroFile.create(druid: druid, filename: 'bar.txt', md5: 'xc157a79031e1c40f85931829bc5fc552', bytes: 0,
@@ -29,19 +38,19 @@ RSpec.describe TechnicalMetadataGenerator do
       it 'generates technical metadata for files' do
         expect(errors.length).to eq(0)
         file1 = DroFile.find_by!(druid: druid, filename: '0001.html')
-        expect(file1.md5).to eq('d41d8cd98f00b204e9800998ecf8427e')
+        expect(file1.md5).to eq('1711cb9f08a0504e1035d198d08edda9')
         expect(file1.filetype).to eq('test')
 
         file2 = DroFile.find_by!(druid: druid, filename: 'bar.txt')
         expect(file2.md5).to eq('c157a79031e1c40f85931829bc5fc552')
-        expect(file2.filetype).to eq('TBD')
+        expect(file2.filetype).to eq('text/plain')
         expect(file2.bytes).to eq(4)
 
         file3 = DroFile.find_by!(druid: druid, filename: 'foo.txt')
         expect(file3.md5).to eq('d3b07384d113edec49eaa6238ad5ff00')
-        expect(file3.filetype).to eq('TBD')
+        expect(file3.filetype).to eq('text/plain')
         expect(file3.bytes).to eq(4)
-        expect(file3.tool_versions).to eq('TBD' => '1.0.0')
+        expect(file3.tool_versions).to eq('siegfried' => '1.4.5')
       end
     end
 
