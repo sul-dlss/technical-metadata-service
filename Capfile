4bac5cf4ac5e5b5bed523a43e1bed23cf996b580
Justin Littman <justinlittman@stanford.edu>
2020-02-19 10:11:38 -0500

    Adds deploy
    closes #5

--- Capfile
+++ Capfile
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
+# Load DSL and set up stages
+require 'capistrano/setup'
+
+# Include default deployment tasks
+require 'capistrano/deploy'
+
+require 'capistrano/scm/git'
+install_plugin Capistrano::SCM::Git
+
+require 'capistrano/bundler'
+# These are waiting for dependencies to be used
+# require 'capistrano/honeybadger'
+require 'capistrano/passenger'
+# require 'capistrano/rails'
+require 'capistrano/rails/migrations'
+require 'dlss/capistrano'
 require 'capistrano/sidekiq'
+
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,48 @@
 # frozen_string_literal: true
 
+set :application, 'technical-metadata-service'
+set :repo_url, 'https://github.com/sul-dlss/technical-metadata-service.git'
+
+# Default branch is :master
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/opt/app/techmd/dor_techmd'
+
+# Since we use webpack rather than sprockets, change the prefix
+# see: http://blog.tap349.com/webpack/2018/05/22/webpack-troubleshooting/
+# set :assets_prefix, 'packs'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :info
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w[config/database.yml config/secrets.yml]
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w[log config/settings public/system tmp/pids]
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# honeybadger_env otherwise defaults to rails_env
+# set :honeybadger_env, fetch(:stage)
+
+# update shared_configs before restarting app
+before 'deploy:restart', 'shared_configs:update'
+
 # Sidekiq configuration (run three processes)
 # see sidekiq.yml for concurrency and queue settings
 set :sidekiq_env, 'production'
