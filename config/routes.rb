baa3091f6648bf8933a05399f1d4c67dd1352f9b
aaron-collier <aaron.collier@stanford.edu>
2020-02-19 14:46:41 -0800

    Add login route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope 'v1' do
     post '/technical-metadata', to: 'technical_metadata#create'
+    post '/auth/login', to: 'authentication#login'
   end
 
   mount Sidekiq::Web => '/queues'
