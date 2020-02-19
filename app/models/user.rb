9d4acbb8efd595902cfda8efd4a915ad2e00e73f
aaron-collier <aaron.collier@stanford.edu>
2020-02-19 14:08:00 -0800

    Add necessary models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+end
