f6191433449a7c05916a2a48ef19142ccc9cc1c9
Justin Coyne <jcoyne@justincoyne.com>
2020-03-25 11:56:57 -0500

    Unescape URLs

--- spec/requests/create_technical_metadata_spec.rb
+++ spec/requests/create_technical_metadata_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'Request create technical metadata' do
       files: [
         'file:///spec/fixtures/test/0001.html',
         'file:///spec/fixtures/test/bar.txt',
-        'file:///spec/fixtures/test/foo.txt'
+        'file:///spec/fixtures/test/foo.txt',
+        'file:///spec/fixtures/test/one%20space.txt'
       ],
       force: true }
   end
@@ -18,12 +19,18 @@ RSpec.describe 'Request create technical metadata' do
   end
 
   context 'when authorized' do
+    let(:filepaths) do
+      ['/spec/fixtures/test/0001.html',
+       '/spec/fixtures/test/bar.txt',
+       '/spec/fixtures/test/foo.txt',
+       '/spec/fixtures/test/one space.txt']
+    end
+
     it 'queues a job' do
       post '/v1/technical-metadata',
            params: data.to_json,
            headers: { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{jwt}" }
 
-      filepaths = ['/spec/fixtures/test/0001.html', '/spec/fixtures/test/bar.txt', '/spec/fixtures/test/foo.txt']
       expect(response).to have_http_status(:ok)
       expect(TechnicalMetadataWorkflowJob).to have_received(:perform_later).with(druid: 'druid:bc123df4567',
                                                                                  filepaths: filepaths, force: true)
