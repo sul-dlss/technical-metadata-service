79fbc496f37bf836b3460031683f3941c593f218
Justin Coyne <jcoyne@justincoyne.com>
2020-04-09 14:02:05 -0500

    Deploy additional workers

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 server 'dor-techmd-prod.stanford.edu', user: 'techmd', roles: %w[web app db]
+server 'dor-techmd-worker-prod-a.stanford.edu', user: 'techmd', roles: %w[app worker]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-server 'dor-techmd-stage.stanford.edu', user: 'techmd', roles: %w[web app db]
+server 'dor-techmd-stage.stanford.edu', user: 'techmd', roles: %w[web app db worker]
+# This server doesn't exist yet.
+# server 'dor-techmd-worker-stage-a.stanford.edu', user: 'techmd', roles: %w[app worker]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
