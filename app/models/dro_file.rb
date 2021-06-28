ac88769e8a0026d95ea8a0fbb29abac93e7b25eb
Justin Littman <justinlittman@stanford.edu>
2021-06-28 10:14:03 -0400

    Switch file counts to approximations.
    closes #257

--- app/models/dro_file.rb
+++ app/models/dro_file.rb
@@ -3,4 +3,12 @@
 # Digital Repository Object File
 class DroFile < ApplicationRecord
   has_many :dro_file_parts, dependent: :destroy
+
+  # Because count is very slow.
+  # See https://www.citusdata.com/blog/2016/10/12/count-performance/
+  def self.estimated_count
+    DroFile.connection.select_all("SELECT n_live_tup FROM pg_stat_all_tables WHERE relname = 'dro_files'")
+           .last['n_live_tup']
+           .round(-4)
+  end
 end
