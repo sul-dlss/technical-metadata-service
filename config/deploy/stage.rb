21c0700cf839438d29dd6e6cc5db873b16c2f5a6
Tony Zanella <tony.zanella@gmail.com>
2021-11-18 16:28:43 -0800

    Decommission techmd-worker-stage-b
      a temporary replacement for worker-stage-a
      while migrating to ubuntu

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,7 +2,6 @@
 
 server 'dor-techmd-stage.stanford.edu', user: 'techmd', roles: %w[web app db worker]
 server 'dor-techmd-worker-stage-a.stanford.edu', user: 'techmd', roles: %w[app worker]
-server 'dor-techmd-worker-stage-b.stanford.edu', user: 'techmd', roles: %w[app worker]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
